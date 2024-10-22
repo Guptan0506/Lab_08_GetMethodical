322889449dc7b6d239022b23d19a28d3c7da546f
Guptan0506 <navyag0509@gmail.com>
2024-10-22 17:48:27 -0400

    commit

--- src/PrettyHeader.java
+++ src/PrettyHeader.java
@@ -5,19 +5,6 @@ public class PrettyHeader {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your message: ");
         String message = scanner.nextLine();
-        prettyHeader(message);
-    }
-
-    public static void prettyHeader(String msg) {
-        int width = 60;
-        int padding = (width - 6 - msg.length()) / 2;
-
-        System.out.println("*".repeat(width));
-        System.out.print("***");
-        System.out.print(" ".repeat(padding));
-        System.out.print(msg);
-        System.out.print(" ".repeat(width - 6 - padding - msg.length()));
-        System.out.println("***");
-        System.out.println("*".repeat(width));
+        SafeInput.prettyHeader(message);
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -121,5 +121,17 @@ public class SafeInput {
         } while (!isValid);
         return response;
     }
+    public static void prettyHeader(String msg) {
+        int width = 60;
+        int padding = (width - 6 - msg.length()) / 2;
+
+        System.out.println("*".repeat(width));
+        System.out.print("***");
+        System.out.print(" ".repeat(padding));
+        System.out.print(msg);
+        System.out.print(" ".repeat(width - 6 - padding - msg.length()));
+        System.out.println("***");
+        System.out.println("*".repeat(width));
+    }
 }
 
