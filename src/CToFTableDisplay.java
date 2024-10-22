ca013aa4589ba7036b203125416d52e8f9669e00
Guptan0506 <navyag0509@gmail.com>
2024-10-22 17:25:42 -0400

    commit

--- src/CToFTableDisplay.java
+++ src/CToFTableDisplay.java
@@ -1,7 +1,7 @@
 public class CToFTableDisplay {
     public static void main(String[] args) {
         System.out.printf("%-10s %-10s\n", "Celsius", "Fahrenheit");
-        System.out.println("------------------------");
+        System.out.println("--------------------");
         for (int c = -100; c <= 100; c++) {
             System.out.printf("%-10d %-10.2f\n", c, CtoF(c));
         }
