179e90593a15189edaf32c10256745fa018b8580
Guptan0506 <navyag0509@gmail.com>
2024-10-22 16:49:09 -0400

    commit

---
+++ src/PrettyHeader.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrettyHeader {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your message: ");
+        String message = scanner.nextLine();
+        prettyHeader(message);
+    }
+
+    public static void prettyHeader(String msg) {
+        int width = 60;
+        int padding = (width - 6 - msg.length()) / 2; // 6 asterisks for padding (3 on each side)
+
+        System.out.println("*".repeat(width));
+        System.out.print("***");
+        System.out.print(" ".repeat(padding));
+        System.out.print(msg);
+        System.out.print(" ".repeat(width - 6 - padding - msg.length()));
+        System.out.println("***");
+        System.out.println("*".repeat(width));
+    }
+}
